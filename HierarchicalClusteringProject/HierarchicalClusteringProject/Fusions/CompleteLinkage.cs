75c67cbee9cd1f831eb28e20b8b3a7b6dc6c2c4b
D3ad5hot <abela_noel@hotmail.com>
2015-02-18 13:34:13 +0100

    added the complete linkage alghorithm

---
+++ HierarchicalClusteringProject/HierarchicalClusteringProject/Fusions/CompleteLinkage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HierarchicalClusteringProject.Fusions
+{
+    public class CompleteLinkage : Fusion
+    {
+        private const float INITIAL_LARGEST_DISTANCE = float.MaxValue;
+
+        public override float CalculateDistance(Cluster cluster1, Cluster cluster2)
+        {
+            float largestDistance = INITIAL_LARGEST_DISTANCE;
+            foreach (Elements elementCluster1 in cluster1)
+            {
+                foreach (Elements elementCluster2 in cluster2)
+                {
+                    float distance = metric.GetDistance(elementCluster1.GetDataPoints(), elementCluster2.GetDataPoints());
+                    if (distance > largestDistance)
+                        largestDistance = distance;
+                }
+            }
+
+            return largestDistance;
+        }
+    }
+}
